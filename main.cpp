a877d590ebcdb8ea4cc74ad86f2cc72b75da181e
Evgenii Fedoseev <evgeniy1294@yandex.ru>
2021-11-29 22:53:42 +0300

    Пример запихивания в lua QVector<Point>

--- main.cpp
+++ main.cpp
@@ -1,48 +1,87 @@
 #include <iostream>
 #include <QVector>
 #include <sol/sol.hpp>
+#include <nlohmann/json.hpp>
 
-static sol::state lua;
+struct Point {
+    Point()
+        : m_azt()
+        , m_el()
+        , m_time() {    }
 
-struct Simple {
-    Simple(): m_val(0) {}
-    Simple(int val) : m_val(val) {}
+    Point(float azt, float el, float time)
+        : m_azt(azt)
+        , m_el(el)
+        , m_time(time) {    }
 
-    int val() { return m_val; }
-    void setVal(int val) { m_val = val; }
+    float azt() const { return m_azt; }
+    float el()  const { return m_el; }
+    float time() const { return m_time; }
+
+    void setAzt(float azt) { m_azt = azt; }
+    void setEl(float el) { m_el = el; }
+    void setTime(float time) { m_time = time; }
 
 private:
-    int m_val;
+    float m_azt;
+    float m_el;
+    float m_time; // May be julian-Time )))
 };
 
-void initLua() {
-    lua.open_libraries(sol::lib::base);
+void printVector(const QVector<Point>& vector) {
+    std::cout << "Print vector: " << std::endl;
+    for(auto& point: vector) {
+        std::cout << "{ " << point.azt() << ", "
+            << point.el() << ", " << point.time() << " }";
+        std::cout << std::endl;
+    }
+
+    std::cout << std::endl;
 }
 
-void test() {
-    std::cout << "test!!!" << std::endl;
-}
+int main(int, char**) {
+    std::cout << "=== containers ===" << std::endl;
 
-void createSimple() {
-    sol::usertype<Simple> simple_type = lua.new_usertype<Simple>("Simple",
-        sol::constructors<Simple(), Simple(int)>());
+    sol::state lua;
+    lua.open_libraries();
 
-    simple_type["val"] = sol::property(&Simple::val, &Simple::setVal);
+    // Add our type to LUA
+    sol::usertype<Point> point_type = lua.new_usertype<Point>("Point",
+        sol::constructors<Point(), Point(float, float, float)>());
+    point_type["azt"]  = sol::property(&Point::azt, &Point::setAzt);
+    point_type["el"]   = sol::property(&Point::el, &Point::setEl);
+    point_type["time"] = sol::property(&Point::time, &Point::setTime);
 
-    return;
-}
+    // Have the function we
+    // just defined in Lua
+    sol::function print_container  = lua["print_container"];
+    sol::function append_container = lua["append_container"];
 
-void createQVector() {
+    // Set a global variable called
+    // "arr" to be a vector of 3 Points
+    lua["arr"] = QVector<Point>{{}, {0.0, 1.1, 0.1}};
 
-}
+    lua.script(R"(
+        p = Point.new()
+        arr:add(p)
+    )");
 
+    QVector<Point>& reference_to_arr = lua["arr"];
 
+    lua.script(R"(
+        p = Point.new(2.0, 2.0, 19.0)
+        arr:add(p)
+    )");
+    printVector(reference_to_arr);
 
-int main() {
-    initLua();
-    createSimple();
+    reference_to_arr.push_back(Point(7.0, 7.0, 999.0));
+    printVector(reference_to_arr);
 
-    lua.script_file("script.lua");
+    lua.script(R"(
+        p = Point.new(99.0, 99.0, 99.0)
+        arr[1] = p
+    )");
+    printVector(reference_to_arr);
 
     return 0;
 }
--- script.lua
+++
@@ -1,7 +0,0 @@
-s1 = Simple.new();
-s2 = Simple.new(5);
-
-print(s1.val);
-
-s2.val = 9;
-print(s2.val);
